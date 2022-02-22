fa87aff0b4d80469b30576bad8c1355c973672f3
Baizid019 <baizidzaman019@gmail.com>
2022-02-22 22:38:49 +0600

    22/2/22

---
+++ Program file/Codeforces/Array Problem/Array Sorting/code using built in function.cpp
@@ -0,0 +1,50 @@
+// code (using buit in function)
+
+
+// for minimum to maximum
+// 3 2 1 input, 1 2 3 output
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     long long int n,i;
+     cin>>n;
+
+     int arr[n];
+
+     for(i=0;i<n;i++)
+     {
+         cin>>arr[i];
+     }
+     
+     sort(arr,arr+n);
+
+     for(i=0;i<n;i++)
+     {
+         cout<<arr[i]<<" ";
+     }
+     cout<<endl;
+    return 0;
+}
+
+
+
+
+// from maximum to minimum
+// 1 2 3 input, 3 2 1 output
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     long long int n,i;
+     cin>>n;
+
+     int arr[n];
+
+     for(i=0;i<n;i++)
+     {
+         cin>>arr[i];
+     }
